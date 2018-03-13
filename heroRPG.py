3d8156d89f701b11746c5de264dbbc5e6395b02d
Bobby Neelon <robert.neelon@gmail.com>
2018-03-13 23:38:32 +0000

    Step1

---
+++ heroRPG.py
@@ -0,0 +1,10 @@
+class Hero:
+    def __init__ (self, health, power):
+        self.health = 10
+        self.power = 5
+        
+class Goblin:
+    def __init__ (self, health, power):
+        self.health = 6
+        self.power = 2
+
