b60e0ff73cdada58a75808d54dab1cac6209c7b8
Bobby Neelon <robert.neelon@gmail.com>
2018-03-14 01:31:03 +0000

    Step 2

--- heroRPG.py
+++ heroRPG.py
@@ -1,10 +1,19 @@
 class Hero:
-    def __init__ (self, health, power):
+    def __init__ (self,name):
+        self.name = name
         self.health = 10
         self.power = 5
+    
+    def attack(self, goblin):
+        goblin.health -= self.power
+        print("{} does {} damage to {}.".format(self.name,self.power,goblin.name))
+        if goblin.health <= 0:
+                print("{} is dead.".format(goblin.name))
+        
+        
         
 class Goblin:
-    def __init__ (self, health, power):
+    def __init__ (self, name):
+        self.name = name
         self.health = 6
         self.power = 2
-
